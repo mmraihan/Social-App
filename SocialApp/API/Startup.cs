cee77ce55dc1257355fefa37d0b14c22ff9a7d59
mmraihan <mm_raihan@live.com>
2022-03-10 23:20:39 +0600

    Add migration and update to Db. Insert data to Sqlite.

--- SocialApp/API/Startup.cs
+++ SocialApp/API/Startup.cs
@@ -18,12 +18,13 @@ namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
 
-        public IConfiguration Configuration { get; }
+       
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,7 +32,7 @@ namespace API
 
             services.AddDbContext<DataContext>(options =>
             {
-                options.UseSqlite("Connection String");
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
             services.AddSwaggerGen(c =>
