c73e056bf9e2e9bffb2f053a9b5a1f6fba5013e4
mmraihan <mm_raihan@live.com>
2022-03-11 00:00:16 +0600

    Add Users Controller.

---
+++ SocialApp/API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _db;
+
+        public UsersController(DataContext db)
+        {
+           _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _db.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _db.Users.Find(id);
+        }
+    }
+}
