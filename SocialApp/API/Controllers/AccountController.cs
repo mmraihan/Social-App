c1aa7884c23724b2be5fef27cdee5d7bbfa145fd
mmraihan <mm_raihan@live.com>
2022-03-11 15:57:42 +0600

    Add Login Endpoint.

--- SocialApp/API/Controllers/AccountController.cs
+++ SocialApp/API/Controllers/AccountController.cs
@@ -49,6 +49,27 @@ namespace API.Controllers
         
         }
 
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+            if (user == null) return Unauthorized("Invalid Username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return user;
+
+
+        }
+
         
     }
 }
