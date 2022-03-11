a900e3b0ff3bff203b0763b37c57cf61361fc4df
mmraihan <mm_raihan@live.com>
2022-03-12 00:04:12 +0600

    Auth: Add Extensions Method to IServiceCollection.

--- SocialApp/API/Startup.cs
+++ SocialApp/API/Startup.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Extensions;
 using API.Interfces;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -34,31 +35,16 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<ITokenService, TokenService>();
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+            services.AddApplicationServices(_config); //--Extension Method
             services.AddControllers();
-            services.AddCors(); //Ordering doesn't matter
+            services.AddCors(); //--Ordering doesn't matter
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                };
-            });
-            }
+            services.AddIdenityServices(_config);  //--Extension Method
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
