d8bb4f07c7edbd3347eff65ffe98cc6cff27a90d
mmraihan <mm_raihan@live.com>
2022-03-12 14:24:14 +0600

    Client Log Reg: Inject service into component.

--- SocialApp/client/src/app/_services/account.service.ts
+++ SocialApp/client/src/app/_services/account.service.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  baseUrl='https://localhost:44328/'
+  baseUrl='https://localhost:44328/api/'
 
   constructor(private http: HttpClient) { }
 
-  logIn(model: any){
-    this.http.post(this.baseUrl+ 'account/login', model);
+  logIn(model: any) : Observable<any>{
+   return this.http.post(this.baseUrl+ 'account/login', model);
   }
 }
--- SocialApp/client/src/app/nav/nav.component.ts
+++ SocialApp/client/src/app/nav/nav.component.ts
@@ -1,4 +1,8 @@
+
 import { Component, OnInit } from '@angular/core';
+
+import { AccountService } from '../_services/account.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -8,14 +12,23 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   model: any={};
+  loggedIn: boolean=false;
 
-  constructor() { }
+
+
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   login(){
-    console.log(this.model);
+    this.accountService.logIn(this.model).subscribe(response=>{
+      console.warn(response);   
+      this.loggedIn=true;
+    },error=>{
+      console.log(error)
+    })
+  }
+ 
   }
 
-}
