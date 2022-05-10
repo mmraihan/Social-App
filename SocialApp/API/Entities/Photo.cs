d4bf4f8d6ed09bd0238740efe4e6b18f12a4018f
mmraihan <mm_raihan@live.com>
2022-05-10 18:04:05 +0600

    Extending the API: DateTime extension to calculate age --API

--- SocialApp/API/Entities/Photo.cs
+++ SocialApp/API/Entities/Photo.cs
@@ -2,5 +2,9 @@
 {
     public class Photo
     {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+        public string PublicId { get; set; }
     }
 }
