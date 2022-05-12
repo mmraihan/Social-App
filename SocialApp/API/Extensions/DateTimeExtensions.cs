204522f6eda8428f3323863442a84ed3d91bb4f5
mmraihan <mm_raihan@live.com>
2022-05-12 14:53:48 +0600

    Extending the API: EF Relationship 1 to Many --API

--- SocialApp/API/Extensions/DateTimeExtensions.cs
+++ SocialApp/API/Extensions/DateTimeExtensions.cs
@@ -5,7 +5,16 @@ using System.Threading.Tasks;
 
 namespace API.Extensions
 {
-    public class DateTimeExtensions
+    public static class DateTimeExtensions
     {
+      public static int CalculateAge(this DateTime dob)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dob.Year;
+            if (dob.Date > today.AddYears(-age)) age--;
+            {
+                return age;
+            }
+        }
     }
 }
