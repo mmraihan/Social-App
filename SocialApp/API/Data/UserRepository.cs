1fee079f1302aa7ab8a3f4552d0108067660cd00
mmraihan <mm_raihan@live.com>
2022-05-27 20:55:09 +0600

    Extending the API: Create Repository for User  --API

---
+++ SocialApp/API/Data/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using API.Entities;
+using API.Interfces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUserNameAsync(string userName)
+        {
+
+            return await _context.Users.SingleOrDefaultAsync(x => x.UserName == userName);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async void Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
---
+++ SocialApp/API/Interfces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUserNameAsync(string userName);
+    }
+}
