d4eeea0e04a571287a2b76bef363249191e76170
mmraihan <mm_raihan@live.com>
2022-05-27 22:52:09 +0600

    Add Automapper --API

---
+++ SocialApp/API/Helpers/AutomapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using API.DTOs;
+using API.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class AutomapperProfiles : Profile
+    {
+        public AutomapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>();
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
