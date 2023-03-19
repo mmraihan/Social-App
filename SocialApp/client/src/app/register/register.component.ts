84e6cf2a1e57b9c5f910727530db8b5a3a37411d
mmraihan <mm_raihan@live.com>
2023-03-19 12:12:47 +0600

    Reactive Form, Add Custom validation.  --Client

--- SocialApp/client/src/app/register/register.component.ts
+++ SocialApp/client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { error } from '@angular/compiler/src/util';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -13,19 +14,38 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
   model:any={};
+  registerForm: FormGroup;
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
   }
+
+  initializeForm(){
+    this.registerForm= new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('',[Validators.required, Validators.minLength(4),Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchvalues('password')]),
+    })
+  }
+
+  //------Custom validation------------
+  matchvalues(matchTo: string): ValidatorFn{
+    return (control: AbstractControl)=>{
+      return control?.value===control?.parent?.controls[matchTo].value ? null : {isMatching:true}
+    }
+  }
+
   register(){
-  this.accountService.register(this.model).subscribe(response=>{
-    console.log(response);
-    this.cancel();
-  },error=>{
-    console.log(error);
-    this.toastr.error(error.error)
-  })
+    console.log(this.registerForm.value)
+  // this.accountService.register(this.model).subscribe(response=>{
+  //   console.log(response);
+  //   this.cancel();
+  // },error=>{
+  //   console.log(error);
+  //   this.toastr.error(error.error)
+  // })
   }  
   cancel(){
     this.cancelRegister.emit(false)
