0c64a126c7841875263eff71c1970b7c486a71a8
mmraihan <mm_raihan@live.com>
2023-03-24 01:12:46 +0600

    Added filter buttons to the clinet.

--- SocialApp/client/src/app/members/member-list/member-list.component.ts
+++ SocialApp/client/src/app/members/member-list/member-list.component.ts
@@ -17,6 +17,7 @@ export class MemberListComponent implements OnInit {
   pagination: Pagination;
   userParams: UserParams;
   user: User;
+  genderList =[{value:'male', display: 'Males'},{value:'female', display:'Females'}]
 
   constructor(
     private memberService: MembersService,
@@ -41,6 +42,11 @@ export class MemberListComponent implements OnInit {
 
   pageChanged(event: any) {
     this.userParams.pageNumber = event.page;
+    this.loadMembers();
+  }
+
+  resetFilters(){
+    this.userParams = new UserParams(this.user);
     this.loadMembers();
   }
 }
