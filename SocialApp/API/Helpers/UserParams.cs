22d0ee170d7e15a71f1e9f406f0c76ab3f123cca
mmraihan <mm_raihan@live.com>
2023-03-24 01:14:33 +0600

    Added  sorting functionality  based on created & lastActive.  --API

--- SocialApp/API/Helpers/UserParams.cs
+++ SocialApp/API/Helpers/UserParams.cs
@@ -16,5 +16,6 @@
         public string Gender { get; set; }
         public int MinAge { get; set; } = 16;
         public int MaxAge { get; set; } = 150;
+        public string OrderBy { get; set; } = "lastActive";
     }
 }
