a2c4d094c52edea69cfe86a242836d34eed518e0
mmraihan <mm_raihan@live.com>
2023-03-28 15:57:29 +0600

    Add User Like Entity winth their configuration. --API

--- SocialApp/API/Controllers/AccountController.cs
+++ SocialApp/API/Controllers/AccountController.cs
@@ -41,8 +41,8 @@ namespace API.Controllers
             user.UserName = registerDto.Username.ToLower();
             user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
             user.PasswordSalt = hmac.Key;
+ 
             
-
             _context.Add(user);
             await _context.SaveChangesAsync();
 
--- SocialApp/API/Data/DataContext.cs
+++ SocialApp/API/Data/DataContext.cs
@@ -13,5 +13,25 @@ namespace API.Data
         {
         }
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(k => new { k.SourceUserId, k.LikedUserId }); //pk
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser) // source user can like many other users
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser) //Liked user can have many liked by users
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
