0055eee42fd474b8e0c9d76af721beeeb97047c3
mmraihan <mm_raihan@live.com>
2023-09-22 15:37:09 +0600

    Query Parameter.

--- SocialApp/client/src/app/members/member-detail/member-detail.component.ts
+++ SocialApp/client/src/app/members/member-detail/member-detail.component.ts
@@ -27,6 +27,10 @@ export class MemberDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMember();
+    this.route.queryParams.subscribe(params =>{
+      params.tab? this.selectTab(params.tab) : this.selectTab(0);
+    })
+  
     this.galleryOptions=[
       {
         width: '500px',
@@ -73,6 +77,11 @@ export class MemberDetailComponent implements OnInit {
         next: messages => this.messages = messages
       })
     }
+  }
+
+ 
+  selectTab(tabId: number){
+    this.memberTabs.tabs[tabId].active = true;
   }
 
 
